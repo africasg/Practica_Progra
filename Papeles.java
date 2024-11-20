57295fd1fd14a7546c8733c280016100e1cfecc0
Africa Sanchez <166367099+africasg@users.noreply.github.com>
2024-11-20 08:58:53 +0100

    Delete Papeles.java

--- Papeles.java
+++
@@ -1,5 +0,0 @@
-public class Papeles extends Objeto { //papeles de herencia
-        public Papeles (String nombreobj, String desc, boolean disponible) {
-            super(nombreobj, desc, disponible);
-        }
-}
